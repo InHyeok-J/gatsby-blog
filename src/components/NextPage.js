1a06e28e2f4e120e382dae8579a7fa316dd64dae
inhyeok <ss112d86@gmail.com>
2021-09-26 20:34:20 +0900

    Feat: 이전글다음글 컴포넌트 반응형구현

--- src/components/NextPage.js
+++ src/components/NextPage.js
@@ -10,37 +10,62 @@ const NextPageWrapper = styled.div`
   justify-content: space-between;
   border-top: 1px solid black;
   padding-top: 20px;
+  transition: all 0.2s ease-in-out;
   a {
     color: rgb(51, 51, 51);
     text-decoration: none;
     height: 100%;
+    width: 100%;
     display: inline-flex;
     justify-content: space-between;
     align-items: center;
-    width: 100%;
+    padding: 0px 10px 0px 10px;
+    box-sizing: border-box;
+  }
+  .page-icon-blcok {
+    width: 20%;
+    svg {
+      font-size: 30px;
+    }
+  }
+  .title-block {
+    width: 80%;
   }
   .border-box {
     height: 80px;
     border: 1px solid rgb(230, 230, 230);
     border-radius: 10px;
-    svg {
-      font-size: 50px;
-    }
+  }
+  .border-box:hover {
+    border: 1px solid rgb(45, 45, 45);
   }
   .container {
-    width: 300px;
+    width: 45%;
     text-align: center;
   }
   .icon {
     color: black;
+    width: 15px;
     position: relative;
-    top: 4px;
+    top: 5px;
+  }
+  .tag {
+    font-size: 0.9rem;
   }
   .next-page-title {
-    width: 250px;
+    max-width: 100%;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
+  }
+
+  @media (max-width: 480px) {
+    .container {
+      width: 50%;
+    }
+    .next-page-title {
+      font-size: 0.9rem;
+    }
   }
 `;
 
@@ -54,12 +79,14 @@ const NextPage = ({ pageContext }) => {
         {previous && (
           <div className="previous-page border-box">
             <Link to={`/post/${previous.slug}`}>
-              <AiOutlineArrowLeft />
-              <div>
+              <div className="page-icon-blcok">
+                <AiOutlineArrowLeft />
+              </div>
+              <div className="title-block">
                 <div className="next-page-title">
                   {previous.frontmatter.title}
                 </div>
-                <div>
+                <div className="tag">
                   <FaTag className="icon" />
                   {previous.frontmatter.tags}
                 </div>
@@ -72,14 +99,16 @@ const NextPage = ({ pageContext }) => {
         {next && (
           <div className="next-page border-box">
             <Link to={`/post/${next.slug}`}>
-              <div>
+              <div className="title-block">
                 <div className="next-page-title">{next.frontmatter.title}</div>
-                <div>
+                <div className="tag">
                   <FaTag className="icon" />
                   {next.frontmatter.tags}
                 </div>
               </div>
-              <AiOutlineArrowRight />
+              <div className="page-icon-blcok">
+                <AiOutlineArrowRight />
+              </div>
             </Link>
           </div>
         )}
